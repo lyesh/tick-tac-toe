eee26bf5831732b9b3bfdfc080d18a940bad84b6
aholmes <ailish.h@verizon.net>
2013-01-23 02:16:24 -0500

    Draw board and tokens now

--- TicTacToe/board.py
+++ TicTacToe/board.py
@@ -1,17 +1,49 @@
 import tkinter as tk
 
 class Board():
-    frame = tk.Frame()
-    frame.grid()
 
     def finish_up(self):
         self.frame.quit()
+
+    def draw_board(self):
+        self.canvas = tk.Canvas(self.frame,width="13c",height="13c")
+        self.canvas.create_rectangle("0.5c","0.5c","12.5c","12.5c")
+        self.canvas.create_line("4.5c","0.5c","4.5c","12.5c")
+        self.canvas.create_line("9c","0.5c","9c","12.5c")
+        self.canvas.create_line("0.5c","4.5c","12.5c","4.5c")
+        self.canvas.create_line("0.5c","9c","12.5c","9c")
+        
+        self.draw_o(0,0)
+        self.draw_x(4,0)
+        
+        self.canvas.grid()
+    
+    def draw_x(self,x,y):
+        x1 = str(x+1)+"c"
+        x2 = str(x+3)+"c"
+        y1 = str(y+1)+"c"
+        y2 = str(y+3)+"c"        
+        self.canvas.create_line(x1,y1,x2,y2)
+        self.canvas.create_line(x2,y1,x1,y2)
+
+    def draw_o(self,x,y):
+        'draw an o at location (x,y). dimensions in cm'
+        x1 = str(x+1)+"c"
+        x2 = str(x+3)+"c"
+        y1 = str(y+1)+"c"
+        y2 = str(y+3)+"c"
+        self.canvas.create_oval(x1,y1,x2,y2)
     
     def __init__(self):
+        self.frame = tk.Frame()
+        self.frame.grid()
+        self.draw_board()
+
         quitButton = tk.Button(self.frame,\
                                text="Quit",\
-                               command=Board.finish_up)
+                               command=self.finish_up)
         quitButton.grid()
+        
         self.frame.mainloop(0)
 
 
--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -1,18 +1,11 @@
-class Location:
-    x = 0
-    y = 0
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-
 class Token:
     shape = ""
-    location = Location(0,0)
-    def __init__(self,x,y):
-        self.location = Location(x,y)
-        
-    def getShape(self):
-        self.shape
+    x = 0
+    y = 0
+    def __init__(self,x,y,shape):
+        self.x=x
+        self.y=y
+        self.shape=shape
 
 class Game:
     
